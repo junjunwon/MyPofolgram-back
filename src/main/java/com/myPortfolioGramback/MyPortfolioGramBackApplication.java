8eaa6e70c898306a40604a5bc9fb470a803c58f2
tg_junho <jh.won@time-gate.com>
2022-03-07 00:50:26 +0900

    init project

---
+++ src/main/java/com/myPortfolioGramback/MyPortfolioGramBackApplication.java
@@ -0,0 +1,13 @@
+package com.myPortfolioGramback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortfolioGramBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortfolioGramBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myPortfolioGramback/MyPortfolioGramBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myPortfolioGramback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioGramBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
