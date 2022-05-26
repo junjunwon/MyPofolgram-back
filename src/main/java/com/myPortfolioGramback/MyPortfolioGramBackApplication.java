4fc0453eb133aede8dea1f10f2b839c81e8eb2a3
won junho <wnsgh1993@gmail.com>
2022-05-26 09:52:04 +0900

    set MultipartConfigElement upload size

--- src/main/java/com/myPortfolioGramback/MyPortfolioGramBackApplication.java
+++ src/main/java/com/myPortfolioGramback/MyPortfolioGramBackApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -15,6 +20,16 @@ public class MyPortfolioGramBackApplication extends SpringBootServletInitializer
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyPortfolioGramBackApplication.class, args);
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//Max size of one file
+		factory.setMaxFileSize(DataSize.parse("1000MB"));
+		//Max Size of All files
+		factory.setMaxRequestSize(DataSize.parse("1000MB"));
+		return factory.createMultipartConfig();
 	}
 
 	@Override
