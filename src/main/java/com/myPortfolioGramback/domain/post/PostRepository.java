084be32dca6b98d1b2ce6b06c83db12d7692727f
won junho <wnsgh1993@gmail.com>
2022-05-27 21:05:21 +0900

    포스트 리스트 조회 시 생성일 기준 내림차순 적용

--- src/main/java/com/myPortfolioGramback/domain/post/PostRepository.java
+++ src/main/java/com/myPortfolioGramback/domain/post/PostRepository.java
@@ -2,8 +2,10 @@ package com.myPortfolioGramback.domain.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
-    List<Post> findAllByUserInfo_id(int userId);
+//    List<Post> findAllByUserInfo_idByOrderByCreateDateDesc(long userId);
+    List<Post> findAllByOrderByCreateDateDesc();
 }
